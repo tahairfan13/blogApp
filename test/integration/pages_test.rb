dda2bd8c203a09490ec005192277becc64fddeee
Taha Ali Irfan <tahairfan1993@gmail.com>
2018-03-20 15:46:47 +0500

    Initial commit

---
+++ test/integration/pages_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class PagesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+ test "should get root" do # will check that is the root route completely set or not
+ 	get root_url
+ 	assert_response :success
+
+ end	
+
+test "should get home" do 
+	get pages_home_url #Where 'pages' is the controller & 'home' is the action
+	assert_response :success #got an error when write 'sucess' REMEMBER should be exact "success"
+
+end	
+
+
+end
